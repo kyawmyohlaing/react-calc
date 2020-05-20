81bc04fe0610a60cfd5450c8b5bb3f84bcc07a0e
kyawmyohlaing <kyawthanthat@gmail.com>
2020-05-20 11:00:30 +0630

    Add float display over input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Button from './components/Button';
 import "./css/style.css";
 
+
 class App extends Component {
 
   constructor(props){
@@ -9,7 +10,7 @@ class App extends Component {
 
     this.state = {
       current: "0",
-      previous: [],
+      previous: []
     }
   }
 
@@ -17,8 +18,15 @@ class App extends Component {
     this.setState({result: 0});
   }
   addToCurrent = (symbol) => {
+    console.log("symbol");
+      if(["/", "-", "+", "*"].indexOf(symbol) > -1){
+        let {previous}=this.state;
+        previous.push(this.state.current + symbol);
+        this.setState({previous});
+      }else{
     this.setState({current: this.state.current + symbol});
   }
+}
   render(){
     const buttons = [
       {symbol:'C', cols:'3', action: this.reset},
@@ -26,7 +34,7 @@ class App extends Component {
       {symbol:'7', cols:'1', action: this.addToCurrent},
       {symbol:'8', cols:'1', action: this.addToCurrent},
       {symbol:'9', cols:'1', action: this.addToCurrent},
-      {symbol:'x', cols:'1', action: this.addToCurrent},
+      {symbol:'*', cols:'1', action: this.addToCurrent},
       {symbol:'4', cols:'1', action: this.addToCurrent},
       {symbol:'5', cols:'1', action: this.addToCurrent},
       {symbol:'6', cols:'1', action: this.addToCurrent},
@@ -43,7 +51,13 @@ class App extends Component {
     ]
     return(
       <div className="App">
-        <input className="result" type="text" value={this.state.current}/>
+        {this.state.previous > 0 ? 
+            <div className="floaty-last">
+              {this.state.previous[this.state.previous.length - 1 ]}
+            </div>
+          :null
+        }
+        <input className="result" type="text" value={this.state.current} />
 
         {buttons.map((btn, i) => {
           return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action(symbol)}/>
